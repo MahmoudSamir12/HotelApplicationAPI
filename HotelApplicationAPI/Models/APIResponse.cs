cb22b5b6a19bafe49b774f7a269755c5b5d8be9a
mahmoudsamiragamy@gmail.com <mahmoudsamiragamy@gmail.com>
2024-11-16 00:12:49 +0200

    No Changes Yet

---
+++ HotelApplicationAPI/Models/APIResponse.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace HotelApplicationAPI.Models
+{
+    public class APIResponse<T>
+    {
+        public bool Success { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public object Error { get; set; }
+
+        public APIResponse(T data, HttpStatusCode statusCode = HttpStatusCode.OK, string message = "")
+        {
+            Success = true;
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+            Error = null;
+        }
+
+        public APIResponse(HttpStatusCode statusCode, string message, object error = null)
+        {
+            Success = false;
+            StatusCode = statusCode;
+            Message = message;
+            Data = default(T);
+            Error = error;
+        }
+    }
+}
---
+++ HotelApplicationAPI/Program.cs
@@ -0,0 +1,52 @@
+
+using HotelApplicationAPI.Connection;
+using Serilog;
+
+namespace HotelApplicationAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            // Initialize Serilog from appsettings.json
+
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services));
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //Connection
+            builder.Services.AddTransient<SqlConnectionFactory>(); // Mahmoud
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
